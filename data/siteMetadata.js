e6db179322e0248266e55c106f4a1be5e415a4ed
Bora Lee <learnerbr@gmail.com>
2024-09-20 16:01:20 +0900

    meta data setup

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,17 +1,16 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Bora Codes Blog',
-  author: 'Bora Lee',
-  headerTitle: 'Bora Codes',
+  title: 'Bora Codes - 보라 코드',
+  author: 'Bora Lee - 이보라',
+  headerTitle: 'Bora Codes - 보라 코드',
   description:
     'Posts written by Bora Lee, a software engineer who loves to learn and share knowledge.',
   language: ['en-us', 'ko'],
-  theme: 'system', // system, dark or light
+  theme: 'system',
   siteUrl: 'https://www.bora.codes',
   siteRepo: 'https://github.com/Violet-Bora-Lee/bora-codes-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
-  // mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'admin@bora.codes',
   github: 'https://github.com/Violet-Bora-Lee',
   x: 'https://x.com/LearnerBR',
@@ -24,33 +23,12 @@ const siteMetadata = {
   // set to true if you want a navbar fixed to the top
   stickyNav: false,
   analytics: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-    umamiAnalytics: {
-      // We use an env variable for this site to avoid other users cloning our analytics ID
-      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
-      // You may also need to overwrite the script if you're storing data in the US - ex:
-      // src: 'https://us.umami.is/script.js'
-      // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
+    clarityAnalytics: {
+      ClarityWebsiteId: process.env.NEXT_PUBLIC_CLARITY_ID,
     },
-    // plausibleAnalytics: {
-    //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    // If you are hosting your own Plausible.
-    //   src: '', // e.g. https://plausible.my-domain.com/js/script.js
-    // },
-    // simpleAnalytics: {},
-    // posthogAnalytics: {
-    //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    // },
-    // googleAnalytics: {
-    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
-    // },
   },
   newsletter: {
-    // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus, beehive
-    // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
+    provider: 'mailchimp',
   },
   comments: {
     // If you want to use an analytics provider you have to add it to the
